c944f7fdffd845330fd61c137a62dc9394a72353
NXTaar <nxtaar@NXTaar-MBP.local>
2017-10-28 21:05:51 +0300

    initial commit for new design

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-require('./module-path');
-require('sequence-based');
-require('./server');
-
-//todo searchWord должно быть минимум три символа длинной
-//todo сохраение ошибок в лог-файлы
